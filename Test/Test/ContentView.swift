082dfaf0b5d7fb619f2398d4ae20e502163899ca
Daniel <d.krivelev@redmadrobot.com>
2022-04-20 20:35:06 +0700

    Init code

---
+++ Test/Test/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Test
+//
+//  Created by Daniel Krivelev on 20.04.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+        .font(.custom("Rubik-Bold", size: 24))
+            .padding()
+      
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Test/Test/TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestApp.swift
+//  Test
+//
+//  Created by Daniel Krivelev on 20.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
